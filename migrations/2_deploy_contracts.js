692ebac89fe95fe05357869445c2aa7d3cc776c8
icarus <bernheim7@yahoo.com>
2019-10-18 21:23:08 +0200

    simple split

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,13 @@
 const Splitter = artifacts.require("Splitter");
-const UserCRUD = artifacts.require("UserCRUD");
-const HitchensUnorderedKeySet = artifacts.require("HitchensUnorderedKeySet");
-const HitchensUnorderedKeySetLib = artifacts.require("HitchensUnorderedKeySetLib");
-
-
+const SafeMath = artifacts.require("SafeMath");
 
 module.exports = function(deployer) {
-  deployer.deploy(HitchensUnorderedKeySetLib);
-  deployer.link(HitchensUnorderedKeySetLib, HitchensUnorderedKeySet);	
-  deployer.deploy(HitchensUnorderedKeySet);
-  deployer.deploy(UserCRUD);
-  deployer.link(HitchensUnorderedKeySet, UserCRUD);
+  // deployer.deploy(HitchensUnorderedKeySetLib);
+  // deployer.link(HitchensUnorderedKeySetLib, HitchensUnorderedKeySet);
+  // deployer.deploy(HitchensUnorderedKeySet);
+  // deployer.deploy(UserCRUD);
+  // deployer.link(HitchensUnorderedKeySet, UserCRUD);
+  deployer.deploy(SafeMath);
   deployer.deploy(Splitter);
-  deployer.link(UserCRUD, Splitter);
+  deployer.link(SafeMath, Splitter);
 };
