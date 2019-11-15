bc8b8a738afe2e1031a1e418cc366c297045f43c
icarus fire <icaruss@icaruss-MacBook-Pro-2.local>
2019-11-15 23:26:47 +0100

    refurbish

--- contracts/EvilSplitterConsumer.sol
+++ contracts/EvilSplitterConsumer.sol
@@ -1,16 +1,18 @@
 pragma solidity 0.5.10;
 
 import "./Splitter.sol";
+import "./SafeMath.sol";
 
 contract EvilSplitterConsumer {
-    event LogConsumerWithdrawFundsEvent(address indexed sender, uint256 amountDrawn);
-    event LogConsumerFundsReceivedEvent(address indexed sender, uint256 amountDrawn);
+    using SafeMath for uint256;
+
+    event LogConsumerFundsReceivedFallbackEvent(address indexed sender, uint256 amountDrawn, uint256 balance);
 
     function() external payable {
-        emit LogConsumerFundsReceivedEvent(msg.sender, msg.value);
+        emit LogConsumerFundsReceivedFallbackEvent(msg.sender, msg.value, address(this).balance);
      }
 
-    function withdrawFunds(Splitter splitter, uint256 amount) public payable {
+    function withdrawFunds(Splitter splitter, uint256 amount) public {
         require (amount > 0, "Withdraw amount should be higher than 0");
         splitter.withdraw(amount);
     }
